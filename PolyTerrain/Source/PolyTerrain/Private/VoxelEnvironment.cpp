6b93bcb58e0bc154481c67b3c8a19a0e11ff4fc3
Edwin Zhang <edwinzhang64@gmail.com>
2017-12-23 00:22:40 -0700

    Basic voxels set up

--- PolyTerrain/Source/PolyTerrain/Private/VoxelEnvironment.cpp
+++ PolyTerrain/Source/PolyTerrain/Private/VoxelEnvironment.cpp
@@ -1,8 +1,10 @@
 // Copyright Edwin Zhang
 
 #include "VoxelEnvironment.h"
-#include "PolyVox/PagedVolume.h"
-#include "PolyVox/MaterialDensityPair.h"
+#include "VM/kernel.h"
+
+using PolyVox::PagedVolume;
+using PolyVox::MaterialDensityPair44;
 
 // Sets default values
 AVoxelEnvironment::AVoxelEnvironment()
@@ -10,6 +12,12 @@ AVoxelEnvironment::AVoxelEnvironment()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	Seed = 128;
+	NoiseOctaves = 3;
+	NoiseFrequency = 0.01f;
+	NoiseScale = 32.f;
+	NoiseOffset = 0.f;
+	EnvironmentHeight = 64.f;
 }
 
 // Called when the game starts or when spawned
@@ -26,3 +34,72 @@ void AVoxelEnvironment::Tick(float DeltaTime)
 
 }
 
+void AVoxelEnvironment::PostInitializeComponents()
+{
+	VoxelVolume = MakeShareable(new PagedVolume<MaterialDensityPair44>(new VoxelEnvironmentPager(Seed, NoiseOctaves, NoiseFrequency, NoiseScale, NoiseOffset, EnvironmentHeight)));
+	Super::PostInitializeComponents();
+}
+
+
+// VoxelEnvironmentPager function definitions
+
+// Constructor
+VoxelEnvironmentPager::VoxelEnvironmentPager(uint32 NoiseSeed, uint32 Octaves, float Frequency, float Scale, float Offset, float Height) : 
+PagedVolume<MaterialDensityPair44>::Pager(), Seed(NoiseSeed), NoiseOctaves(Octaves), NoiseFrequency(Frequency), NoiseScale(Scale), NoiseOffset(Offset), EnvironmentHeight(Height)
+{
+		
+}
+
+// Called when when new chunk paged in
+void VoxelEnvironmentPager::pageIn(const PolyVox::Region& region, PagedVolume<MaterialDensityPair44>::Chunk* Chunk)
+{
+	anl::CKernel NoiseKernel;
+	auto Zero = NoiseKernel.constant(0);
+	auto One = NoiseKernel.constant(1);
+	auto VerticalHeight = NoiseKernel.constant(EnvironmentHeight);
+
+	// Gradient on vertical axis for ground plane
+	auto VerticalGradient = NoiseKernel.divide(NoiseKernel.clamp(NoiseKernel.subtract(VerticalHeight, NoiseKernel.z()), Zero, VerticalHeight), VerticalHeight);
+
+	// Turn gradient into two solids that represent ground and air
+	auto VerticalSelect = NoiseKernel.select(Zero, One, VerticalGradient, NoiseKernel.constant(0.5), Zero);
+
+	// Noise simple fBm generator for hills
+	auto EnvironmentFractal = NoiseKernel.simplefBm(anl::BasisTypes::BASIS_SIMPLEX, anl::InterpolationTypes::INTERP_LINEAR, NoiseOctaves, NoiseFrequency, Seed);
+
+	// Scale and offset the noise values
+	auto EnvironmentScale = NoiseKernel.scaleOffset(EnvironmentFractal, NoiseScale, NoiseOffset);
+
+	// Turn fractal into heightmap by setting height to Zero
+	auto EnvironmentZScale = NoiseKernel.scaleZ(EnvironmentScale, Zero);
+
+	//Apply the Z offset from fractal to ground plane.
+	auto ApplyOffset = NoiseKernel.translateZ(VerticalSelect, EnvironmentZScale);
+
+	anl::CNoiseExecutor EnvironmentExecutor(NoiseKernel);
+
+	// Loop over chunk to apply noise
+	for (int x = region.getLowerX(); x <= region.getUpperX(); x++)
+	{
+		for (int y = region.getLowerY(); y <= region.getUpperY(); y++)
+		{
+			for (int z = region.getLowerZ(); z <= region.getUpperZ(); z++) { 
+				auto EvaluatedNoise = EnvironmentExecutor.evaluateScalar(x, y, z, ApplyOffset); 
+				MaterialDensityPair44 Voxel; 
+				bool bSolid = EvaluatedNoise > 0.5;
+
+				Voxel.setDensity(bSolid ? 255 : 0);
+				Voxel.setMaterial(bSolid ? 1 : 0);
+
+				// Subtract lower corner position of the region from volume space position to get chunk space position
+				Chunk->setVoxel(x - region.getLowerX(), y - region.getLowerY(), z - region.getLowerZ(), Voxel);
+			}
+		}
+	}
+}
+
+// Called when chunk is paged out
+void VoxelEnvironmentPager::pageOut(const PolyVox::Region& region, PagedVolume<MaterialDensityPair44>::Chunk* Chunk)
+{
+
+}
--- PolyTerrain/Source/PolyTerrain/Public/VoxelEnvironment.h
+++ PolyTerrain/Source/PolyTerrain/Public/VoxelEnvironment.h
@@ -2,9 +2,45 @@
 
 #pragma once
 
+#include "PolyVox/PagedVolume.h"
+#include "PolyVox/MaterialDensityPair.h"
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "VoxelEnvironment.generated.h"
+
+class VoxelEnvironmentPager : public PolyVox::PagedVolume<PolyVox::MaterialDensityPair44>::Pager
+{
+public:
+	// Constructor
+	VoxelEnvironmentPager(uint32 NoiseSeed = 128, uint32 Octaves = 3, float Frequency = 0.01, float Scale = 32, float Offset = 0, float Height = 64);
+
+	// Destructor
+	virtual ~VoxelEnvironmentPager() {};
+
+	// PagedVolume::Pager functions
+	virtual void pageIn(const PolyVox::Region& region, PolyVox::PagedVolume<PolyVox::MaterialDensityPair44>::Chunk* pChunk);
+	virtual void pageOut(const PolyVox::Region& region, PolyVox::PagedVolume<PolyVox::MaterialDensityPair44>::Chunk* pChunk);
+
+private:
+	// Fractal seed
+	uint32 Seed = 128;
+
+	// Octaves for noise generator
+	uint32 NoiseOctaves = 3;
+
+	// Noise frequency
+	float NoiseFrequency = 0.01;
+
+	// Multiplier for output
+	float NoiseScale = 32;
+	
+	// Offset added to output
+	float NoiseOffset = 0;
+
+	// Maximum height of generated environment in voxels
+	float EnvironmentHeight = 64;
+};
+
 
 UCLASS()
 class POLYTERRAIN_API AVoxelEnvironment : public AActor
@@ -15,6 +51,27 @@ public:
 	// Sets default values for this actor's properties
 	AVoxelEnvironment();
 
+	// Called after C++ constructor and after property initialization
+	virtual void PostInitializeComponents() override;
+
+	// Fractal seed
+	UPROPERTY(Category = "Voxel Environment", BlueprintReadWrite, EditAnywhere) int32 Seed;
+
+	// Octaves for noise generator
+	UPROPERTY(Category = "Voxel Environment", BlueprintReadWrite, EditAnywhere) int32 NoiseOctaves;
+
+	// Noise frequency
+	UPROPERTY(Category = "Voxel Environment", BlueprintReadWrite, EditAnywhere) float NoiseFrequency;
+
+	// Multiplier for fractal output
+	UPROPERTY(Category = "Voxel Environment", BlueprintReadWrite, EditAnywhere) float NoiseScale;
+
+	// Offset added to fractal output
+	UPROPERTY(Category = "Voxel Environment", BlueprintReadWrite, EditAnywhere) float NoiseOffset;
+
+	// Maximum height of generated environment in voxels
+	UPROPERTY(Category = "Voxel Environment", BlueprintReadWrite, EditAnywhere) float EnvironmentHeight;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
@@ -23,6 +80,7 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-	
+private:
+	TSharedPtr<PolyVox::PagedVolume<PolyVox::MaterialDensityPair44>> VoxelVolume;
 	
 };
